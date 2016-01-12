c9596daa3ecf8a215718df9bd9de35723754e86f
n0andyes <n0andyes@gmail.com>
2016-01-12 09:44:44 -0600

    Create gitlab.sh

---
+++ gitlab.sh
@@ -0,0 +1,9 @@
+sudo docker run --detach \
+    --hostname git.nixshroud.io \
+    --publish 32794:80 \
+    --name gitlab \
+    --restart always \
+    --volume /srv/gitlab/config:/etc/gitlab \
+    --volume /srv/gitlab/logs:/var/log/gitlab \
+    --volume /srv/gitlab/data:/var/opt/gitlab \
+    gitlab/gitlab-ce:latest
